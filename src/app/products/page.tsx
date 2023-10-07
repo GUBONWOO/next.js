acd27d6a58b6469f987132b5498c28a1339399d4
GUBONWOO <zcwxzsx@gmail.com>
2023-10-07 15:02:57 +0900

    end of rendering

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,20 @@
 import { getProducts } from '@/service/products';
 // import exp from 'constants';
 import Link from 'next/link';
+import styles from './page.module.css';
+import MeowArticle from '@/components/MeowArticle';
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    next: {
+      revalidate: 0,
+    },
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지</h1>
@@ -16,6 +25,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
