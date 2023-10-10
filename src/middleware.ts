2a49a8e468e28aafe036814cdda5151f4f2e478f
GUBONWOO <zcwxzsx@gmail.com>
2023-10-10 13:38:18 +0900

    s

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   console.log('working');
   if (request.nextUrl.pathname.startsWith('/products/1004')) {
+    console.log('미들우ㅠㅔ어');
     return NextResponse.redirect(new URL('/products', request.url));
   }
 }
